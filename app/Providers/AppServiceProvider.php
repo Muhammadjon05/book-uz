cd8119b6ce0aa1dbab801cd09bc7a3efa57103b9
Muhammadjon05 <muhammadjonmakhkamboyev@gmail.com>
2023-12-31 10:17:12 +0500

    namespaces removed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,12 +2,6 @@
 
 namespace App\Providers;
 
-use App\Repositories\BookRepository\BookRepository;
-use App\Repositories\BookRepository\IBookRepository;
-use App\Repositories\CategoryRepository\CategoryRepository;
-use App\Repositories\CategoryRepository\ICategoryRepository;
-use App\Repositories\GenericRepositories\GenericRepository;
-use App\Repositories\GenericRepositories\IGenericRepository;
 use App\Repositories\UserRepository\IUserRepository;
 use App\Repositories\UserRepository\UserRepository;
 use Illuminate\Support\ServiceProvider;
